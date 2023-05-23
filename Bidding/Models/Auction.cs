8511ef811ac244291e912fc60ea6c1c5fdf82fe8
AsgerHL <asgeman@gmail.com>
2023-05-23 15:30:49 +0200

    initial

---
+++ Bidding/Models/Auction.cs
@@ -0,0 +1,20 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Bidding.Models;
+
+public class Auction
+{
+    
+[BsonElement("_id")]
+[BsonId]
+public ObjectId Id{get; set;}
+public int AuctionId{get; set;}
+public string? auctioneerId{get; set;}
+public int ItemId{get; set;}
+public DateTime? StartDate{get; set;}
+public DateTime? EndDate{get; set;}
+public int MinBid{get; set;}
+public int NextBid{get; set;} 
+public List<Bid> bids{get; set;}
+}
